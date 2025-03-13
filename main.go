906b4ff060cf3b27de1a18c8a9b9904773916a42
whynayemnay <pixeldja@gmail.com>
2025-03-13 15:30:22 +0100

    improved error handling, interactions

--- main.go
+++ main.go
@@ -9,17 +9,21 @@ import (
 
 func main() {
 	reader := bufio.NewScanner(os.Stdin)
-	fmt.Print("Pokedex > ")
-	for reader.Scan() {
-		line := reader.Text()
-		trim := cleanInput(line)
-		word := trim[0]
+	for {
+		fmt.Print("Pokedex > ")
+		if !reader.Scan() {
+			break
+		}
+
+		line := cleanInput(reader.Text())
+		if len(line) == 0 {
+			continue
+		}
+		word := line[0]
 		fmt.Printf("Your command was: %s\n", word)
 		if err := reader.Err(); err != nil {
 			fmt.Println("error reading input: ", err)
 		}
-		fmt.Print("Pokedex > ")
-
 	}
 }
 

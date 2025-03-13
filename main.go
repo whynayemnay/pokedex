63d571c8bde13d3c88466f9d4bda1324f98c022b
whynayemnay <pixeldja@gmail.com>
2025-03-13 15:07:27 +0100

    repl cli functionality

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for reader.Scan() {
+		line := reader.Text()
+		trim := cleanInput(line)
+		word := trim[0]
+		fmt.Printf("Your command was: %s\n", word)
+		if err := reader.Err(); err != nil {
+			fmt.Println("error reading input: ", err)
+		}
+		fmt.Print("Pokedex > ")
+
+	}
 }
 
 func cleanInput(text string) []string {
